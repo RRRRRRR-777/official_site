077b9bcc8e6f63813eeaad81e74937777d4fd705
RRRRRRR-7777 <yr1919@icloud.com>
2022-07-28 10:17:00 +0900

    デプロイ後でもエラーメッセージを見れるようにする

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import redirect, render
+# デプロイ後でもエラーメッセージを見れるようにする
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
 
-from django.http import HttpResponse
-from django.conf import settings
-from django.core.mail import BadHeaderError, send_mail
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
+
+
+from django.shortcuts import render
 
 
 def index(request):
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,9 @@
+# デプロイ後でもエラーメッセージを見れるようにする
+from shop import views
+
+handler500 = views.my_customized_server_error
+
+
 from django.contrib import admin
 from django.urls import include, path
 
