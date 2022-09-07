cf0dcbc5ec78802ce6e39d6253c63d7dcdbd0975
RRRRRRR-7777 <yr1919@icloud.com>
2022-09-07 11:32:53 +0900

    change email to gmail

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,11 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # # メールサーバー用
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.ocn.ne.jp'
-EMAIL_PORT = 465
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 #Heroku databaseaa
