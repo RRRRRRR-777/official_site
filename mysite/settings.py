95164d817b58a0bc9f4f65a2f189dbc8a0889582
RRRRRRR-7777 <yr1919@icloud.com>
2022-09-07 11:29:33 +0900

    change email to ocn

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,12 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # # メールサーバー用
-EMAIL_HOST = 'smtp.gmail.com'
-
-
-# EMAIL_HOST_USER = 'raiseha.nekogaii777@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ikxrodxsvoeksomt'
-EMAIL_PORT = 587
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.ocn.ne.jp'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 #Heroku databaseaa
