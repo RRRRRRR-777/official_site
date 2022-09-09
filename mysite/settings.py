4cb116d4f457dcf72747144f892a2ecfc0a78aac
RRRRRRR-7777 <yr1919@icloud.com>
2022-09-09 09:58:17 +0900

    modified footer, textbox

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# # メールサーバー用
+# メールサーバー用 gmail
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
@@ -162,16 +162,6 @@ if not DEBUG:
  
  AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
  AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-
-# メールサーバー用
-# EMAIL_HOST = 'smtp.ocn.ne.jp'
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = 465
-# EMAIL_USE_TLS = True
-
-
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
