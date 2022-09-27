af2339117cef2daf6be13c74dedfbe7ee5cab11e
RRRRRRR-7777 <yr1919@icloud.com>
2022-09-27 09:23:05 +0900

    modified robots.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,10 @@
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('robots.txt', TemplateView.as_view(template_name='static/robots.txt', content_type='text/plain')),  # <- append
 ]
-
-from django.conf.urls.static import static
-from django.urls import include, path
-from django.views.generic import TemplateView
-
-path('robots.txt', TemplateView.as_view(template_name='static/robots.txt', content_type='text/plain')),
