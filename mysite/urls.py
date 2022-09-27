930f106f653f9c7dfff6dece3900fa5319d7eef6
RRRRRRR-7777 <yr1919@icloud.com>
2022-09-27 09:16:20 +0900

    add robots.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+
+from django.conf.urls.static import static
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+path('robots.txt', TemplateView.as_view(template_name='static/robots.txt', content_type='text/plain')),
